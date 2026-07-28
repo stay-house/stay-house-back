53416063370e3c77e28f96b0a6b4c7a7f4476914
hyojin425 <gywls5184@gmail.com>
2026-07-28 15:53:40 +0900

    first commit

---
+++ src/main/java/com/example/stay_house_back/StayHouseBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.stay_house_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayHouseBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayHouseBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stay_house_back/StayHouseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stay_house_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayHouseBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
